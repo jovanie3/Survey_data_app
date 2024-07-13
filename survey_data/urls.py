9f2d5465ada3df6b3238ce76cfe374e907011b8e
Essimbi <essimbideranot@gmail.com>
2024-07-13 20:12:37 +0100

    Mise à jour de l'urls.py(formatage de la requête), du fichier views.py(chargement et le rendu du template) et mise à jour du type de paramètre de la requête.

--- survey_data/urls.py
+++ survey_data/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from survey_data.views import display_survey_data
 
 urlpatterns = [
-    path('/display_survey_data/<uuid:survey_id>/', display_survey_data, name='display_survey_data'),
-    
+    path('display_survey_data/<str:survey_id>', display_survey_data, name='display_survey_data'),   
 ]
